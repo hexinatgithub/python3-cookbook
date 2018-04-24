f480db82dbf1b2d87edead97e07c9a6156692e73
hexin <hexin.work@outlook.com>
2018-04-24 14:58:20 +0800

    9.9. Defining Decorators As Classes

---
+++ Chapter9/9_9.py
@@ -0,0 +1,42 @@
+import types
+from functools import wraps
+class Profiled:
+    def __init__(self, func):
+        wraps(func)(self)
+        self.ncalls = 0
+    
+    def __call__(self, *args, **kwargs): 
+        self.ncalls += 1
+        return self.__wrapped__(*args, **kwargs)
+
+    def __get__(self, instance, cls): 
+        if instance is None:
+            return self 
+        else:
+            return types.MethodType(self, instance)
+
+@Profiled
+def add(x, y): 
+    return x + y
+
+class Spam: 
+    @Profiled
+    def bar(self, x): 
+        print(self, x)
+
+    """
+    Equal to:
+    s = Spam()
+    def bar(self, x):
+        print(self, x)
+    bar.__get__(s, Spam)
+    """
+
+add(2, 3)
+add(4, 5)
+add.ncalls
+s = Spam()
+s.bar(1)
+s.bar(2)
+s.bar(3)
+Spam.bar.ncalls
