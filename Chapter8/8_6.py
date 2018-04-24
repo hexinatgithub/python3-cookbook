acaaed91275636c1fc7eda4a0b0c1c7565da90a0
hexin <hexin.work@outlook.com>
2018-04-24 21:06:38 +0800

    8.6. Creating Managed Attributes

---
+++ Chapter8/8_6.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
+        # Getter function
+
+    @property
+    def first_name(self): 
+        return self._first_name
+
+    # Setter function
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string') 
+        self._first_name = value
+        
+    # Deleter function (optional)
+    @first_name.deleter
+    def first_name(self):
+        raise AttributeError("Can't delete attribute")
