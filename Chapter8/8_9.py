601ebe950a944d35055a90ec2849ee8c42ea52ba
hexin <hexin.work@outlook.com>
2018-04-28 18:33:36 +0800

    8.9. Creating a New Kind of Class or Instance Attribute

---
+++ Chapter8/8_9.py
@@ -0,0 +1,72 @@
+# Descriptor attribute for an integer type-checked attribute
+class Integer:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an int')
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+
+class Point:
+    x = Integer('x')
+    y = Integer('y')
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# p = Point(2, 3)
+# p.x  # Calls Point.x.__get__(p, Point)
+# p.y = 5  # Calls Point.y.__set__(p, 5)
+# Point.x  # Calls Point.x.__get__(None, Point)
+
+# Descriptor for a type-checked attribute
+class Typed:
+    def __init__(self, name, expected_type):
+        self.name = name
+        self.expected_type = expected_type
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('Expected ' + str(self.expected_type))
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+
+def typeassert(**kwargs):
+    # Class decorator that applies it to selected attributes
+    def decorate(cls):
+        for name, expected_type in kwargs.items():
+            # Attach a Typed descriptor to the class
+            setattr(cls, name, Typed(name, expected_type))
+        return cls
+    return decorate
+
+
+# Example use
+@typeassert(name=str, shares=int, price=float)
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
---
+++ Chapter8/8_9_extend.py
@@ -0,0 +1,111 @@
+# https://docs.python.org/3/howto/descriptor.html
+import types
+
+
+class C(object):
+    def getx(self):
+        return self.__x
+
+    def setx(self, value):
+        self.__x = value
+
+    def delx(self):
+        del self.__x
+
+    x = property(getx, setx, delx, "I'm the 'x' property.")
+
+
+# To see how property() is implemented in terms of the descriptor protocol, here is a pure Python equivalent.
+class Property(object):
+    "Emulate PyProperty_Type() in Objects/descrobject.c"
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+    def getter(self, fget):
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+    def setter(self, fset):
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+    def deleter(self, fdel):
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+
+class D(object):
+    def f(self, x):
+        return x
+
+
+# Equal to D class has a Function object in class dict.
+class Function(object):
+    def __get__(self, obj, objtype=None):
+        "Simulate func_descr_get() in Objects/funcobject.c"
+        if obj is None:
+            return self
+        return types.MethodType(self, obj)
+
+
+class E(object):
+    def f(x):
+        print(x)
+    f = staticmethod(f)
+
+
+# Equal to E class has a StaticMethod object in Class dict.
+class StaticMethod(object):
+    "Emulate PyStaticMethod_Type() in Objects/funcobject.c"
+
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, objtype=None):
+        return self.f
+
+
+class Dict(object):
+    def fromkeys(klass, iterable, value=None):
+        "Emulate dict_fromkeys() in Objects/dictobject.c"
+        d = klass()
+        for key in iterable:
+            d[key] = value
+        return d
+    fromkeys = classmethod(fromkeys)
+
+
+# Equal to Dict class has a ClassMethod object in Class dict.
+class ClassMethod(object):
+    "Emulate PyClassMethod_Type() in Objects/funcobject.c"
+
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+
+        def newfunc(*args):
+            return self.f(klass, *args)
+        return newfunc
