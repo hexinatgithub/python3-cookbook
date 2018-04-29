634964050d03b7044959e2ae55f3af40dea85bf4
hexin <hexin.work@outlook.com>
2018-04-29 13:52:30 +0800

    8.18. Extending Classes with Mixins

---
+++ Chapter8/8_18.py
@@ -0,0 +1,75 @@
+from collections import defaultdict
+
+
+class LoggedMappingMixin:
+    '''
+    Add logging to get/set/delete operations for debugging.
+    '''
+    __slots__ = ()
+
+    def __getitem__(self, key):
+        print('Getting ' + str(key))
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        print('Setting {} = {!r}'.format(key, value))
+        return super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        print('Deleting ' + str(key))
+        return super().__delitem__(key)
+
+
+class SetOnceMappingMixin:
+    '''
+    Only allow a key to be set once.
+    '''
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise KeyError(str(key) + ' already set')
+        return super().__setitem__(key, value)
+
+
+class StringKeysMappingMixin:
+    '''
+    Restrict keys to strings only
+    '''
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, str):
+            raise TypeError('keys must be strings')
+        return super().__setitem__(key, value)
+
+
+class LoggedDict(LoggedMappingMixin, dict):
+    pass
+
+
+class SetOnceDefaultDict(SetOnceMappingMixin, defaultdict):
+    pass
+
+
+def LoggedMapping(cls):
+    cls_getitem = cls.__getitem__
+    cls_setitem = cls.__setitem__
+    cls_delitem = cls.__delitem__
+
+    def __getitem__(self, key):
+        print('Getting ' + str(key))
+        return cls_getitem(self, key)
+
+    def __setitem__(self, key, value):
+        print('Setting {} = {!r}'.format(key, value))
+        return cls_setitem(self, key, value)
+
+    def __delitem__(self, key):
+        print('Deleting ' + str(key))
+        return cls_delitem(self, key)
+
+    cls.__getitem__ = __getitem__
+    cls.__setitem__ = __setitem__
+    cls.__delitem__ = __delitem__
+    return cls
