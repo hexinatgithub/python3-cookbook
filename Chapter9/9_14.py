4019ccb4de4305225d617662e7f1ce3aafb57131
hexin <hexin.work@outlook.com>
2018-04-30 16:40:29 +0800

    9.14. Capturing Class Attribute Definition Order

---
+++ Chapter9/9_14.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict
+
+
+# A set of descriptors for various types
+class Typed:
+    _expected_type = type(None)
+
+    def __init__(self, name=None):
+        self._name = name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self._expected_type):
+            raise TypeError('Expected ' + str(self._expected_type))
+        instance.__dict__[self._name] = value
+
+
+class Integer(Typed):
+    _expected_type = int
+
+
+class Float(Typed):
+    _expected_type = float
+
+
+class String(Typed):
+    _expected_type = str
+
+
+# Metaclass that uses an OrderedDict for class body
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        order = []
+        for name, value in clsdict.items():
+            if isinstance(value, Typed):
+                value._name = name
+                order.append(name)
+        d['_order'] = order
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return OrderedDict()
+
+
+class Structure(metaclass=OrderedMeta):
+    def as_csv(self):
+        return ','.join(str(getattr(self, name)) for name in self._order)
+
+
+# Example use
+class Stock(Structure):
+    name = String()
+    shares = Integer()
+    price = Float()
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+# The entire key to this recipe is the __prepare__() method, which is defined in the
+# OrderedMeta metaclass. This method is invoked immediately at the start of a class def‐
+# inition with the class name and base classes. It must then return a mapping object to
+# use when processing the class body. By returning an OrderedDict instead of a normal
+# dictionary, the resulting definition order is easily captured.
