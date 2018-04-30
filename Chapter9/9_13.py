7143745cc8963d83c2ab817f382cdc81e81da4bc
hexin <hexin.work@outlook.com>
2018-04-30 16:22:42 +0800

    9.13. Using a Metaclass to Control Instance Creation

---
+++ Chapter9/9_13.py
@@ -0,0 +1,41 @@
+class Singleton(type):
+    def __init__(self, *args, **kwargs):
+        self.__instance = None
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__(*args, **kwargs)
+            return self.__instance
+        else:
+            return self.__instance
+
+
+# Example
+class Spam(metaclass=Singleton):
+    def __init__(self):
+        print('Creating Spam')
+
+
+import weakref
+
+
+class Cached(type):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__cache = weakref.WeakValueDictionary()
+
+    def __call__(self, *args):
+        if args in self.__cache:
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+            return obj
+
+
+# Example
+class Spam(metaclass=Cached):
+    def __init__(self, name):
+        print('Creating Spam({!r})'.format(name))
+        self.name = name
